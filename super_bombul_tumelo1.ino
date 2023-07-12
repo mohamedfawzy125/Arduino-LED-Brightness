e9f2d7b08ec85bef235e4dc48ec7e1ecba629121
mohamedfawzy125 <138731256+mohamedfawzy125@users.noreply.github.com>
2023-07-12 15:21:08 +0300

    Update super_bombul_tumelo1.ino

--- super_bombul_tumelo1.ino
+++ super_bombul_tumelo1.ino
@@ -6,10 +6,13 @@ void setup()
 }
 
 void loop()
-{
-  analogWrite(9, 50);
+{ 
+  for(int i=0 ; i <=255 ; i++){
+  analogWrite(9, i);
 delay(500);
-
+  } 
+  for(int i=255 ; i >=0 ; i--){
  analogWrite(9, 255);
    delay(1000);
+  }
 }
